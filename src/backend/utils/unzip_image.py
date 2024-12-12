323b4baa9be3a89c1c052928b545eb2184dd397e
maymilla <164962489+MaylaYL@users.noreply.github.com>
2024-12-13 00:21:28 +0700

    add: unzipMusic

--- src/backend/utils/unzip_image.py
+++ src/backend/utils/unzip_image.py
@@ -2,7 +2,6 @@ import zipfile
 import os
 
 def unzipImage(zip_folder, hasil_folder) :
-
     # ambil direktori dimana file dijalankan
     script_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -13,14 +12,15 @@ def unzipImage(zip_folder, hasil_folder) :
     if not os.path.exists(extract_folder):
         os.makedirs(extract_folder)
 
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        zip_ref.printdir()
+        image_extensions = ['.jpg', '.jpeg', '.png', '.raw', '.bmp', '.tiff']
 
-        image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff']
+        try:
+            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+                zip_ref.printdir()
 
-        for file in zip_ref.namelist():
-            # Check if the file is an image based on its extension
-            if any(file.lower().endswith(ext) for ext in image_extensions):
-                # Extract the image to the specified folder
-                zip_ref.extract(file, extract_folder)
-                print(f'{file} has been extracted to {extract_folder}')
+                for file in zip_ref.namelist():
+                    if any(file.lower().endswith(ext) for ext in image_extensions): # cek secara case-insensitive
+                        zip_ref.extract(file, extract_folder)
+                        print(f'{file} has been extracted to {extract_folder}')
+        except FileNotFoundError as e:
+            print(f"Error: {e}")
