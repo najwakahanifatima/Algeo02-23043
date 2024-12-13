4a3e7bde265fd1eec2244d96fc749d4e16339933
maymilla <164962489+MaylaYL@users.noreply.github.com>
2024-12-13 13:33:47 +0700

    fix: notifikasi

--- src/backend/api/main.py
+++ src/backend/api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, File, Form, UploadFile
 import os
+import sys
 from ..utils.unzip_music import unzipMusic 
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 app = FastAPI()
 
 # Directory where audio and image folders will be created
@@ -16,7 +18,8 @@ async def handle_database_audio(
     file: UploadFile = File(...), folder_name: str = Form(...)
 ):
     # Create a folder for audio files
-    zip_file_path = os.path.join(BASE_DIR, "uploaded_files", file.filename)
+    zip_file_path = os.path.join(BASE_DIR, "src/backend/database_music/uploaded_files", file.filename)
+    print(zip_file_path)
     os.makedirs(zip_file_path, exist_ok=True)
 
     # Save the uploaded file in the audio folder
